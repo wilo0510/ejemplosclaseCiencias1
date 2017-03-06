9891fa762f1c14e8d119b2f8635761f8da5b1962
wilo0510 <wrpachonl@gmail.com>
2017-03-06 07:31:18 -0500

    Update ejemploestructura.cpp

--- ejemploestructura.cpp
+++ ejemploestructura.cpp
@@ -5,7 +5,7 @@ struct Nodo{
 	Nodo *siguiente;
 };
 class ListaDinamica{
-	private:	
+	protected:
 	Nodo *cabeza;
 	public:
 	ListaDinamica(){
@@ -33,7 +33,7 @@ Nodo *ListaDinamica::buscar(int elemento){
 		}
 		p=p->siguiente;
 	}
-	return p;
+	return NULL;
 }
 void ListaDinamica::mostrar(){
 	Nodo *p=cabeza;
@@ -59,14 +59,62 @@ void ListaDinamica::eliminar(int elemento){
 	Nodo *p=cabeza;
 	Nodo *temp;
 	if(p->elemento==elemento){
-		cabeza = *p->siguiente;
+		cabeza = p->siguiente;
 		delete p;
 	}else{
-		while(p)
+		//while(p)
 	}
 }
 bool ListaDinamica::vacia(){
 	return cabeza==NULL;
+}
+class Cola:public ListaDinamica{
+	private:
+		Nodo * fin;
+	public:
+		Cola(){
+			cabeza=fin=NULL;
+		}
+		void agregar(int);
+		int eliminar();
+};
+
+void Cola::agregar(int elemento)
+{
+		Nodo *p=new Nodo;
+		(*p).elemento=elemento;
+		p->siguiente=NULL;
+		if(cabeza)
+		fin->siguiente=p;
+		else
+		cabeza=p;
+		fin=p;
+}
+int Cola::eliminar()
+{
+	int elemento=cabeza->elemento;
+	Nodo *p=cabeza;
+	cabeza=cabeza->siguiente;
+	delete p;
+	return elemento;
+}
+class Pila:public ListaDinamica
+{	
+
+	public:
+		Pila()
+		{
+			cabeza=NULL;
+		}
+		int eliminar();
+};
+int Pila::eliminar()
+{
+	int elemento=cabeza->elemento;
+	Nodo *p=cabeza;
+	cabeza=cabeza->siguiente;
+	delete p;
+	return elemento;
 }
 char menu(){
 	char opcion;
@@ -82,7 +130,7 @@ char menu(){
 	return opcion;			
 }
 int main(){
-	ListaDinamica lista;
+	Cola lista;
 	char opcion;
 	int elemento;
 	do{opcion=menu();
@@ -102,11 +150,27 @@ int main(){
 			case 'B':
 				cout<<"Ingrese el elemento a buscar \n";
 				cin>>elemento;
-				cout<<"El elemento se encuentra en "<<lista.buscar(elemento);	
+				if(lista.buscar(elemento))
+				{
+					cout<<"El elemento se encuentra en "<<lista.buscar(elemento);
+				}
+				else
+				{
+					cout<<"no esta";
+				}
+					
 				break;
 			case 'e':
 			case 'E':
-				cout<<"Eliminar ultimo elemento.El elemento eliminado es: "<<lista.eliminar();			
+				if(!lista.vacia())
+				{
+					elemento=lista.eliminar();
+					cout<<"Eliminar ultimo elemento.El elemento eliminado es: "<<lista.eliminar();		
+				}
+				else
+				{
+					cout<<"No hay elementos para eliminar";
+				}
 				break;		
 		}
 	}while(opcion!='s' && opcion!='S');
