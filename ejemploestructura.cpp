7859f65310f0fbcdd04396f74ba82a3219db712a
wilo0510 <wrpachonl@gmail.com>
2017-03-06 06:41:03 -0500

    Update ejemploestructura.cpp

--- ejemploestructura.cpp
+++ ejemploestructura.cpp
@@ -3,102 +3,114 @@ using namespace std;
 struct Nodo{
 	int elemento;
 	Nodo *siguiente;
-	};
+};
 class ListaDinamica{
-	private:
-	
+	private:	
 	Nodo *cabeza;
-	
-	
 	public:
-	ListaDinamica()
-		{
+	ListaDinamica(){
 		cabeza=NULL;
-		}
-		
+	}	
 	void agregar(int);
 	void mostrar();
 	int eliminar();
 	void eliminar(int);
 	Nodo *buscar(int);
 	void ordenar();
-	};
-	
+	bool vacia();
+};	
 void ListaDinamica::agregar(int elemento){
 	Nodo *p=new Nodo;
 	(*p).elemento=elemento;
 	(*p).siguiente=cabeza;
 	cabeza=p;
-	}
-Nodo *ListaDinamica::buscar(int elemento)
-	{
-		Nodo *p=cabeza;
-				while(p!=NULL)
-		{
-			if(p->elemento==elemento)
-			{
-				
-				return p;
-			}
-			p=p->siguiente;
+}
+Nodo *ListaDinamica::buscar(int elemento){
+	Nodo *p=cabeza;
+	while(p!=NULL){
+		if(p->elemento==elemento){
+			return p;
 		}
-		return p;
+		p=p->siguiente;
 	}
-void ListaDinamica::mostrar()
-	{
-		Nodo *p=cabeza;
-		while(p!=NULL)
-		{
-			cout<<p->elemento<<" ";
-			p=p->siguiente;
-		}
-			
+	return p;
+}
+void ListaDinamica::mostrar(){
+	Nodo *p=cabeza;
+	while(p!=NULL){
+		cout<<p->elemento<<" ";
+		p=p->siguiente;
+	}		
+}
+int ListaDinamica::eliminar(){
+	int info;
+	Nodo *p=cabeza;
+	Nodo *q=cabeza->siguiente;
+	while(q->siguiente!=NULL){
+		p=q;
+		q=q->siguiente;
 	}
-	char menu()
-	{
-		char opcion;
-		cout<<"\n MENU \n";
-		cout<<"(A)gregar \n";
-		cout<<"(E)liminar \n";
-		cout<<"(M)ostrar \n";
-		cout<<"(B)uscar \n";
-		cout<<"(O)rdenar \n";
-		cout<<"(S)alir \n";
-		cout<<"Escoja una opcion: ";
-		cin>>opcion;
-		return opcion;
-				
-		}
+	p->siguiente=NULL;
+	info = q->elemento;
+	delete q;
+	return info;
+}
+void ListaDinamica::eliminar(int elemento){
+	Nodo *p=cabeza;
+	Nodo *temp;
+	if(p->elemento==elemento){
+		cabeza = *p->siguiente;
+		delete p;
+	}else{
+		while(p)
+	}
+}
+bool ListaDinamica::vacia(){
+	return cabeza==NULL;
+}
+char menu(){
+	char opcion;
+	cout<<"\n MENU \n";
+	cout<<"(A)gregar \n";
+	cout<<"(E)liminar \n";
+	cout<<"(M)ostrar \n";
+	cout<<"(B)uscar \n";
+	cout<<"(O)rdenar \n";
+	cout<<"(S)alir \n";
+	cout<<"Escoja una opcion: ";
+	cin>>opcion;
+	return opcion;			
+}
 int main(){
 	ListaDinamica lista;
 	char opcion;
 	int elemento;
 	do{opcion=menu();
-		switch(opcion)
-		{
+		switch(opcion){
 			case'a':
-			case'A':cout<<"Digite el elemento: ";
-					cin>>elemento;
-					lista.agregar(elemento);
-					break;
+			case'A':
+				cout<<"Digite el elemento: ";
+				cin>>elemento;
+				lista.agregar(elemento);
+				break;
 			case 'm':
-			case 'M':cout<<"\n Los elementos almacenados ; \n";
-					lista.mostrar();
-					break;
+			case 'M':
+				cout<<"\n Los elementos almacenados ; \n";
+				lista.mostrar();
+				break;
 			case 'b':
-			case 'B':cout<<"Ingrese el elemento a buscar \n";
-					cin>>elemento;
-					cout<<"El elemento se encuentra en "<<lista.buscar(elemento);
-					
-					break;
-					
+			case 'B':
+				cout<<"Ingrese el elemento a buscar \n";
+				cin>>elemento;
+				cout<<"El elemento se encuentra en "<<lista.buscar(elemento);	
+				break;
+			case 'e':
+			case 'E':
+				cout<<"Eliminar ultimo elemento.El elemento eliminado es: "<<lista.eliminar();			
+				break;		
 		}
 	}while(opcion!='s' && opcion!='S');
 	lista.agregar(20);
 	cout<<"Elementos almacenados :\n";
 	lista.mostrar();
-	lista.agregar(45);
-	lista.agregar(19);
-	cout<<"Elementos almacenados :\n";
-	lista.mostrar();
-	}
+}
